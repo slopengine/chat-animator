3b43cf142536ab605df72b381579ed819e9bdd3b
slopengine <slopengine@gmail.com>
2026-01-26 10:07:16 -0800

    chore: add .gitignore and remove node_modules from tracking

--- node_modules/@remotion/renderer/dist/browser/handle-failed-resource.d.ts
+++
@@ -1,9 +0,0 @@
-import type { HTTPRequest } from './HTTPRequest';
-import type { LoadingFailedEvent, ResponseReceivedExtraInfoEvent } from './devtools-types';
-export declare const handleFailedResource: ({ extraInfo, logLevel, indent, request, event, }: {
-    extraInfo: ResponseReceivedExtraInfoEvent[];
-    logLevel: "error" | "info" | "trace" | "verbose" | "warn";
-    indent: boolean;
-    request: HTTPRequest;
-    event: LoadingFailedEvent;
-}) => void;
--- node_modules/@remotion/studio/dist/components/RenderModal/WebRenderModal.d.ts
+++
@@ -1,3 +0,0 @@
-import type { WebRenderModalState } from '../../state/modals';
-export type RenderType = 'still' | 'video';
-export declare const WebRenderModalWithLoader: React.FC<WebRenderModalState>;
